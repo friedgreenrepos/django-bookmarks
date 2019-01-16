766e7674c9ea669738062e75e6a716bd064a58f7
gccallie <12759493+gccallie@users.noreply.github.com>
2019-01-16 11:52:20 +0100

    update bookmark visibility

--- bookmarks/views/mixins.py
+++ bookmarks/views/mixins.py
@@ -3,9 +3,12 @@ from django.http import HttpResponse
 from django.urls import resolve
 from ..models import Bookmark
 from ..forms import BookmarkForm
+from ..settings import BUTTON_TEXT_SAVE, BUTTON_TEXT_DELETE
+from .. import utils
+
 
 class BookmarkMixin:
-    ''' Provides a BookmarkForm instance in the context '''
+    ''' Provide a BookmarkForm instance in the context '''
     bookmark_form = 'bookmark_form'
 
     def get_bookmark_form_name(self):
@@ -13,19 +16,34 @@ class BookmarkMixin:
 
     def get_context_data(self):
         context = super().get_context_data()
+        context['button_text'] = BUTTON_TEXT_SAVE
+        context['bookmark_exists'] = False
         match = resolve(self.request.path)
-        initials = {
-            'urlname': match.url_name,
-            'args': match.args if match.args else None,
-            'kwargs': match.kwargs if match.kwargs else None,
-            'urlparams': self.request.GET.urlencode(),
-        }
-        context[self.get_bookmark_form_name()] = BookmarkForm(initial=initials)
+        params = self.request.GET.urlencode()
+        if params:
+            context['bookmark_bnt_visible'] = True
+            initials = {
+                'urlname': match.url_name,
+                'args': match.args if match.args else None,
+                'kwargs': match.kwargs if match.kwargs else None,
+                'urlparams': params,
+            }
+            context[self.get_bookmark_form_name()] = BookmarkForm(initial=initials)
 
-        bm_id = self.request.GET.get('bm', None)
-        if bm_id is not None:
-            context['bookmark_exists'] = Bookmark.objects.filter(id=bm_id).exists()
-            context['current_bm_id'] = bm_id
+            filters = {
+                'user': self.request.user,
+                'urlname': match.url_name,
+                'args': match.args if match.args else None,
+                'kwargs': match.kwargs if match.kwargs else None,
+            }
+            for bm in Bookmark.objects.filter(**filters):
+                params_dict = utils.params_to_dict(bm.urlparams)
+                if cmp(params_dict, self.request.GET.dict()) ==  0:
+                    context['bookmark_exists'] = True
+                    context['current_bm_id'] = bm.id
+
+            if context['bookmark_exists']:
+                context['button_text'] = BUTTON_TEXT_DELETE
         return context
 
 
