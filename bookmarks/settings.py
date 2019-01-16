6a282258da6c2c6e5e16513801ec15780ae8eb72
gccallie <12759493+gccallie@users.noreply.github.com>
2019-01-16 11:47:27 +0100

    add constants in settings

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -1,1 +1,19 @@
 from django.conf import settings
+
+BUTTON_TEXT_SAVE = getattr(
+    settings,
+    'BOOKMARKS_BUTTON_TEXT_SAVE',
+    'Salva nei Bookmark',
+)
+
+BUTTON_TEXT_DELETE = getattr(
+    settings,
+    'BOOKMARKS_BUTTON_TEXT_DELETE',
+    'Rimuovi dai Bookmark',
+)
+
+BOOKMARK_FORM_NAME = getattr(
+    settings,
+    'BOOKMARKS_BOOKMARK_FORM_NAME',
+    'Nome',
+)
